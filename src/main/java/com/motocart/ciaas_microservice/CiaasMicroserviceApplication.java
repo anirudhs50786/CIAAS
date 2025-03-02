b93c716b94b1adcda1dc6775eeff0ef7e2d100d5
anirudh sreekumar <anirudhs50786@gmail.com>
2025-03-02 11:28:57 +0530

    initial commit

---
+++ src/main/java/com/motocart/ciaas_microservice/CiaasMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.motocart.ciaas_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaasMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiaasMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motocart/ciaas_microservice/CiaasMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motocart.ciaas_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaasMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
